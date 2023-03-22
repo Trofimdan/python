9bf450e794aef579cc344e4e7d6b6766cfcf28c4
Trofimdan <128606181+Trofimdan@users.noreply.github.com>
2023-03-22 16:20:21 +0100

    Add files via upload

---
+++ guess number.py
@@ -0,0 +1,9 @@
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+number = input("Choose a difficulty. Type 'easy' or 'hard':")
+if number == 'easy':
+    print("You have 10 attempts remaining to guess the number.")
+for i in range(1,101):
+    if i
+    
+    
